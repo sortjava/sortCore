8c8712cfd05172e270fe0a1e82031bc0733d3adb
sandesh <sandesh061987@gmail.com>
2021-09-27 11:14:20 +0530

    Updating email and reset confirmation text and design

--- src/main/java/com/sort/sortcore/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/sort/sortcore/security/jwt/AuthEntryPointJwt.java
@@ -1,16 +1,15 @@
 package com.sort.sortcore.security.jwt;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
@@ -20,12 +19,12 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        logger.error("Unauthorized error: {}", authException.getMessage());
-        if("User is disabled" == authException.getMessage().toString()) {
+        if ("User is disabled" == authException.getMessage().toString()) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Your account is not activated yet. Kindly check your email and confirm your registration by clicking on account verification link.");
-        } else if ( "Bad credentials" == authException.getMessage().toString()) {
+        } else if ("Bad credentials" == authException.getMessage().toString()) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: User email or Password is incorrect");
         } else {
+            logger.error("Unauthorized error: {}", authException.getMessage());
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
         }
     }
